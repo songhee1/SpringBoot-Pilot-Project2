7db08931a20ab85f9dae539f2f107443d20108d1
songhee1 <ghenrhkwk88@gmail.com>
2025-04-30 14:26:54 +0900

    index controller add

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public @ResponseBody String index(){
+        return "Hello World";
+        // 문자 출력
+    }
+
+    
+}
